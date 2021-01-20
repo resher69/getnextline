43be52acfdeed110b11619a0a5d7841c669b7c62
Kali <resher.ag@gmail.com>
2021-01-20 18:49:16 -0500

    work home

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agardet <agardet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: kali <kali@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/20 11:07:16 by agardet           #+#    #+#             */
-/*   Updated: 2021/01/20 16:05:44 by agardet          ###   ########lyon.fr   */
+/*   Updated: 2021/01/20 18:48:22 by kali             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,12 @@ static char	*ft_save_line(char *buf)
 	size_t	i;
 	
 	while (buf[i] && buf[i] != '\n')
-	{
-		save[i] = buf[i];
 		i++;
-	}
+	if (!(save = malloc(sizeof(char) * i + 1)))
+		return (NULL);
 	save[i] = 0;
+	while (--i >= 0)
+		save=[i] = buf[i];
 	return(save);
 }
 
@@ -38,11 +39,23 @@ static char	*ft_rest_line(char *buf)
 	while (buf[i])
 	{
 		if (buf[i] == '\n')
+		{
+			if (!(rest = maloc(sizeof(char))))
+				return (NULL);
 			while (buf[i])
 				rest[j++] == buf[i++];
+			
 		i++;
 	}
+	rest[j] = 0;
 	return (rest);
+}
+
+int		ft_error_handler(char *save, char *buf)
+{
+	free(save);
+	free(buf);
+	return (-1);
 }
 
 int		get_next_line(int fd, char **line)
